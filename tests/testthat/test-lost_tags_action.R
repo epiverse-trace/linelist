769b2f728c3a1d6234f32c2706d70a8cdf36530a
Hugo Gruson <Bisaloo@users.noreply.github.com>
2023-10-10 14:25:20 +0200

    Add deprecation tests

--- tests/testthat/test-lost_tags_action.R
+++ tests/testthat/test-lost_tags_action.R
@@ -17,3 +17,18 @@ test_that("tests for lost_tags_action", {
   lost_tags_action("warning", quiet = TRUE)
   expect_identical(get_lost_tags_action(), "warning")
 })
+
+test_that("deprecated pipeline option for lost_tags_action()", {
+
+  x <- make_linelist(cars, date_onset = "dist", date_outcome = "speed")
+  expect_error(
+    lost_tags_action(x),
+    "deprecated"
+  )
+  expect_error(
+    lost_tags_action(
+      x = "none"
+    )
+  )
+
+})
