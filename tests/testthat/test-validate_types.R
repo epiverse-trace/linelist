1c48ff1a0b777454f973515d695427489e42cd90
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2024-02-20 10:58:18 +0100

    Move validate_type() functionality to validate_types()

--- tests/testthat/test-validate_types.R
+++ tests/testthat/test-validate_types.R
@@ -1,33 +1,30 @@
-test_that("tests for validate_types", {
-
-  # test errors
-  msg <- "Must inherit from class 'linelist', but has class 'data.frame'."
-  expect_error(validate_types(cars), msg)
-
-  x <- make_linelist(cars, age = "speed")
-  msg <- paste(
-    "Issue when checking class of tag `age`:",
-    "Must inherit from class 'factor', but has class 'numeric'",
-    sep = "\n"
+test_that("tests for validate_types() basic input checking", {
+  expect_error(
+    validate_types(cars), 
+    "Must inherit from class 'linelist', but has class 'data.frame'."
   )
-  expect_error(validate_types(x, ref_types = tags_types(age = "factor")), msg)
+})
+
+test_that("validate_types() validates types", {
+  # Successful validations
+  x <- make_linelist(cars, age = "speed")
+  expect_silent(
+    expect_identical(
+      x,
+      validate_types(x)
+    )
+  )
+
+  # Failed validations
+  x <- make_linelist(cars, age = "speed")
+  expect_error(
+    validate_types(x, ref_types = tags_types(age = "factor")), 
+    "age: Must inherit from class 'factor', but has class 'numeric'"
+  )
 
   x <- make_linelist(cars, age = "speed", gender = "dist")
-  msg <- paste(
-    "Issue when checking class of tag `gender`:",
-    "Must inherit from class 'character'/'factor', but has class 'numeric'",
-    sep = "\n"
-  )
-  expect_error(validate_types(x), msg)
-
-  # test functionalities
-  x <- make_linelist(cars, age = "speed")
-  expect_identical(x, validate_types(x))
-
-  x <- make_linelist(cars, age = "speed", outcome = "dist")
-  expect_identical(
-    x,
-    validate_types(x, ref_types = tags_types(outcome = "numeric"))
+  expect_snapshot_error(
+    validate_types(x, ref_types = tags_types(age = "factor"))
   )
 })
 
