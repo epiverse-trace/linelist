a94572d6230318826ad19c51a959bf09f5d78835
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2024-02-21 12:35:01 +0100

    Upgrade test warnings to errors

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,8 @@
-if (require(testthat)) {
-  library(linelist)  # nolint
-  test_check("linelist")
-} else {
-  warning("'linelist' requires 'testthat' for tests")
+library(testthat)
+library(linelist)
+
+test_results <- test_check("linelist")
+
+if (any(as.data.frame(test_results)$warning > 0)) {
+  stop("tests failed with warnings")
 }
