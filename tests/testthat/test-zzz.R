1a66adde799b008b6aa8d22886ffa55cf469401b
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2024-04-23 16:02:49 +0200

    Add test for new behaviour

--- tests/testthat/test-zzz.R
+++ tests/testthat/test-zzz.R
@@ -1,3 +1,5 @@
+skip_if_not_installed("callr")
+
 test_that("tests for zzz", {
   # We need to use callr to avoid conflicts with other tests
   res <- callr::r(
@@ -7,4 +9,18 @@ test_that("tests for zzz", {
     }
   )
   expect_identical(res, "warning")
+
+})
+
+test_that("Environment variable is used for initial `lost_tags_action`", {
+  # We need to use callr to avoid conflicts with other tests
+  res <- callr::r(
+    function() {
+      library(linelist)
+      get_lost_tags_action()
+    },
+    env = c(LINELIST_LOST_ACTION = "error")
+  )
+  expect_identical(res, "error")
+
 })
