ad371832659ddbbf36fc6b31d8e6c06e7f068f1b
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2024-04-27 21:29:58 +0200

    Fix if condition for tests warnings promotion
    Co-authored-by: Kirill Müller <krlmlr@users.noreply.github.com>

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,6 +3,6 @@ library(linelist)
 
 test_results <- as.data.frame(test_check("linelist"))
 
-if (any(test_results$warning > 0) && Sys.getenv("NOT_CRAN", FALSE)) {
+if (any(test_results$warning > 0) && !identical(Sys.getenv("NOT_CRAN"), "TRUE")) {
   stop("tests failed with warnings")
 }
