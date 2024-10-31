71769a5fa607a847f19ac56a56133b315f40b597
Chris Hartgerink <chris@libscie.org>
2024-10-31 10:26:11 +0100

    Update `modify_defaults` to use labels
    Now we check not based on names of the list, but values.
    This represents the change to using labels instead of tags.

--- R/modify_defaults.R
+++ R/modify_defaults.R
@@ -3,30 +3,53 @@
 #' Internal function. This is used to modify default values provided in a named
 #' list.
 #'
-#' @author Code by Rich FitzJohn borrowed from outbreaker2
-#'
 #' @param defaults a named list of default values
 #'
 #' @param x a named list of modified values, to replace the defaults
 #'
-#' @param strict a `logical` indicating if allowed modification should be
-#'   restricted to variables existing in the defaults (`TRUE`); if `FALSE`, new
-#'   variables will allowed and added to the output
+#' @param allow_extra a `logical` indicating if allowed modification should be
+#'   restricted to variables existing in the defaults; if `TRUE`, new
+#'   variables will be allowed and added to the output
 #'
 #' @noRd
 #'
 #' @return A named `list`.
 
-modify_defaults <- function(defaults, x, strict = TRUE) {
-  extra <- setdiff(names(x), names(defaults))
-  if (strict && (length(extra) > 0L)) {
-    stop(
-      "Unknown variable types: ",
-      toString(extra),
-      "\n  ",
-      "Use only tags listed in `tags_names()`, or set `allow_extra = TRUE`",
-      call. = FALSE
-    )
+modify_defaults <- function(defaults, x, allow_extra = FALSE) {
+  checkmate::assert_list(defaults)
+  checkmate::assert_list(x, unique = TRUE)
+  # Create a copy of defaults to modify
+  result <- defaults
+  
+  # 1. Find the values in x that match the values in defaults
+  # Currently case sensitive, can improve by making case insensitive
+  matching_labels <- intersect(unlist(x), unlist(defaults))
+  
+  # 2. Replace the names in defaults, of those matching elements, with the names
+  # of the matching elements in x
+  for (label in matching_labels) {
+    match <- defaults == label
+    matchX <- x == label
+    if (sum(match) > 1) stop("Duplicates present for default labels. Please
+                             provide unique variables for labels and remove any
+                             duplicates.")
+    names(result)[match] <- names(x)[matchX]
   }
-  utils::modifyList(defaults, x, keep.null = TRUE) # keep.null is needed here
+  
+  # 3. If allow_extra is FALSE and there are elements in x that are not in
+  # defaults, throw an error
+  if (!allow_extra) {
+    extra_elements <- setdiff(unlist(x), unlist(defaults))
+    if (length(extra_elements) > 0) {
+      stop("There are labels in x that are not in defaults and allow_extra is FALSE")
+    }
+  }
+  
+  # 4. If allow_extra is TRUE and there are elements in x that are not in
+  # defaults, add them to the result
+  if (allow_extra) {
+    extra_elements <- setdiff(x, defaults)
+    result <- c(result, extra_elements)
+  }
+  return(result)
 }
---
+++ tests/testthat/test-modify_defaults.R
@@ -0,0 +1,44 @@
+test_that("modify_defaults successfully updates defaults with matching elements from x", {
+  defaults <- list(a = "Healthcare worker", b = "Date of symptom onset")
+  x <- list(hcw = "Healthcare worker", date_onset = "Date of symptom onset")
+  result <- modify_defaults(defaults, x, allow_extra = TRUE)
+
+  expect_equal(result, x)
+})
+
+test_that("modify_defaults successfully updates with extras", {
+  defaults <- list(a = "Healthcare worker", b = "Date of symptom onset")
+  x <- list(hcw = "Healthcare worker", 
+            date_onset = "Date of symptom onset",
+            date_reporting = "Date of reporting to health authorities")
+  result <- modify_defaults(defaults, x, allow_extra = TRUE)
+  expect_equal(result, x)
+})
+
+# Test for expected failure
+test_that("modify_defaults throws an error when allow_extra is FALSE and x has extra elements", {
+  defaults <- list(a = "Healthcare worker", b = "Date of symptom onset")
+  x <- list(hcw = "Healthcare worker", 
+            date_onset = "Date of symptom onset",
+            date_reporting = "Date of reporting to health authorities")
+  
+  expect_error(
+    modify_defaults(defaults, x),
+    "There are labels in x that are not in defaults and allow_extra is FALSE"
+  )
+})
+
+test_that("modify_defaults throws an error when x
+          has duplicate labels", {
+  defaults <- list(a = "Healthcare worker", b = "Date of symptom onset")
+  x <- list(hcw = "Healthcare worker", 
+            d = "Healthcare worker",
+            date_onset = "Date of symptom onset",
+            date_reporting = "Date of reporting to health authorities")
+  
+  expect_error(
+    modify_defaults(defaults, x, allow_extra = TRUE),
+    "Assertion on 'x' failed: Contains duplicated values, position 2."
+  )
+})
+
