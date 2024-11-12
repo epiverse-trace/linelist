a57dce6645ea2151c98847ead1ac865d2c5ab202
Chris Hartgerink <chris@libscie.org>
2024-11-12 11:16:39 +0100

    Update modify_defaults to handle new data format

--- R/modify_defaults.R
+++ R/modify_defaults.R
@@ -20,11 +20,11 @@ modify_defaults <- function(defaults, x, allow_extra = FALSE) {
   checkmate::assert_list(x, unique = TRUE)
   # Create a copy of defaults to modify
   result <- defaults
-  
+
   # 1. Find the values in x that match the values in defaults
   # Currently case sensitive, can improve by making case insensitive
   matching_labels <- intersect(unlist(x), unlist(defaults))
-  
+
   # 2. Replace the names in defaults, of those matching elements, with the names
   # of the matching elements in x
   for (label in matching_labels) {
@@ -33,9 +33,10 @@ modify_defaults <- function(defaults, x, allow_extra = FALSE) {
     if (sum(match) > 1) stop("Duplicates present for default labels. Please
                              provide unique variables for labels and remove any
                              duplicates.")
+    print(x)
     names(result)[match] <- names(x)[matchX]
   }
-  
+
   # 3. If allow_extra is FALSE and there are elements in x that are not in
   # defaults, throw an error
   if (!allow_extra) {
@@ -44,7 +45,7 @@ modify_defaults <- function(defaults, x, allow_extra = FALSE) {
       stop("There are labels in x that are not in defaults and allow_extra is FALSE")
     }
   }
-  
+
   # 4. If allow_extra is TRUE and there are elements in x that are not in
   # defaults, add them to the result
   if (allow_extra) {
