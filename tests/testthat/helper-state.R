1572b360b19c014b2e72e16c519e9b75dcada890
Bisaloo <10783929+Bisaloo@users.noreply.github.com>
2025-05-28 15:38:15 +0000

    Update config files from packagetemplate

--- tests/testthat/helper-state.R
+++ tests/testthat/helper-state.R
@@ -3,20 +3,33 @@
 # If global settings need to be modified, they should be restored to their
 # original values on exit. This can be achieved with the `on.exit()` base
 # function, or more conveniently with the `withr` package.
-testthat::set_state_inspector(function() {
-  list(
-    attached    = search(),
-    connections = getAllConnections(),
-    cwd         = getwd(),
-    envvars     = Sys.getenv(),
-    handlers    = globalCallingHandlers(),
-    libpaths    = .libPaths(),
-    locale      = Sys.getlocale(),
-    options     = options(),
-    par         = par(),
-    packages    = .packages(all.available = TRUE),
-    sink        = sink.number(),
-    timezone    = Sys.timezone(),
-    NULL
-  )
-})
+# We add a test on R >= 4.0.0 because some functions such as
+# `globalCallingHandlers()` did not exist before.
+get_pars_toreset <- function() {
+  pars <- par(no.readonly = TRUE)
+  # The following params are set and modified automatically by plot(), as
+  # documented in ?plot.window() and we:
+  # 1. have no control over them
+  # 2. do not care about resetting them
+  pars <- pars[!names(pars) %in% c("usr", "xaxp", "yaxp")]
+}
+
+if (getRversion() >= "4.0.0") {
+  testthat::set_state_inspector(function() {
+    list(
+      attached    = search(),
+      connections = getAllConnections(),
+      cwd         = getwd(),
+      envvars     = Sys.getenv(),
+      handlers    = globalCallingHandlers(),
+      libpaths    = .libPaths(),
+      locale      = Sys.getlocale(),
+      options     = options(),
+      par         = get_pars_toreset(),
+      packages    = .packages(all.available = TRUE),
+      sink        = sink.number(),
+      timezone    = Sys.timezone(),
+      NULL
+    )
+  })
+}
